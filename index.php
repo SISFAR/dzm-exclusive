deca23aba0ca00fed7791433a39f7b334d649576
SISFAR <47912952+SISFAR@users.noreply.github.com>
2021-03-02 10:26:08 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,6 @@
-<?php?>
-<!DOCTYPE html>
 <html>
-<head>
-    <meta charset='utf-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>D'ZM Exclusive</title>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='style.css'>
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Potta+One&display=swap" rel="stylesheet">
-    <script src='main.js'></script>
-</head>
-<body>
-    <div class="signup-form">
-        <img src="user.png" alt="dzmexclusive">
-        <form action="index.php" method="post">
-            <input type="text" placeholder="Username" class="txt" name="username">
-            <input type="email" placeholder="Email" class="txt" name="email">
-            <input type="password" placeholder="Password" class="txt" name="password">
-            <input type="button" value="Create a Account" class="btn" name="btn-login">
-        </form>
-    </div>
-</body>
+    <head></head>
+    <body>
+        <p> hello </p>
+    </body>
 </html>
